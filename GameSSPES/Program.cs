f68eccd6ba873dfa1b4bb4c8c4a24d27fd8905fd
AlexQuintanaGarcia <alejandro.quintana@gmx.com>
2020-01-04 15:49:36 +0100

    Add project files.

---
+++ GameSSPES/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameSSPES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
