c8b3f3a7b95b82a21df491c3577caeb2b3ca3fc1
AlexQuintanaGarcia <alejandro.quintana@gmx.com>
2020-01-04 18:59:06 +0100

    UD

--- GameSSPES/Program.cs
+++ GameSSPES/Program.cs
@@ -11,9 +11,11 @@ namespace GameSSPES
             Console.WriteLine("Die Regeln: \nSchere schneidet Papier, \nPapier bedeckt Stein, \nStein zerquetscht Echse, \nEchse vergiftet Spock, \nSpock zertrümmert Schere, \nSchere köpft Echse, \nEchse frisst Papier, \nPapier widerlegt Spock, \nSpock verdampft Stein. \nUnd wie gewöhnlich – Stein schleift Schere. \n ");
             Console.Write("Bitte schreiben Sie Ihre Name: ");
             string userName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hallo {0}, bitte wählen Sie ein nummer: 1. Schere, 2. Stein, 3. Papier, 4. Echse, 5. Spock", userName);
 
 
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
