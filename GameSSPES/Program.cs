0c5949c27604b31eb0565a302bb73ec245c8ea93
AlexQuintanaGarcia <alejandro.quintana@gmx.com>
2020-01-04 18:42:50 +0100

    Update

--- GameSSPES/Program.cs
+++ GameSSPES/Program.cs
@@ -6,7 +6,14 @@ namespace GameSSPES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(@"Wilkommen bei Schere, Stein, Papier, Echse, Spock
+                ");
+            Console.WriteLine("Die Regeln: \nSchere schneidet Papier, \nPapier bedeckt Stein, \nStein zerquetscht Echse, \nEchse vergiftet Spock, \nSpock zertrümmert Schere, \nSchere köpft Echse, \nEchse frisst Papier, \nPapier widerlegt Spock, \nSpock verdampft Stein. \nUnd wie gewöhnlich – Stein schleift Schere. \n ");
+            Console.Write("Bitte schreiben Sie Ihre Name: ");
+            string userName = Console.ReadLine();
+
+
+            Console.ReadLine();
         }
     }
 }
